85d26d000f8528834f2a8de6597e1578450fef8c
Ouch.Liu <ouch.liu@xuenn.com>
2019-06-13 16:53:43 +0800

    Refactor unit test

--- Otp/OtpUnitTest/AuthenticationServiceTests.cs
+++ Otp/OtpUnitTest/AuthenticationServiceTests.cs
@@ -7,49 +7,48 @@ namespace OtpUnitTest
     public class AuthenticationServiceTests
     {
         private AuthenticationService _authenticationService;
+        private IProfile _profileForTest;
+        private IToken _tokenForTest;
 
+        private string _account;
+        private string _password;
 
         [SetUp]
         public void Setup()
         {
+            _profileForTest = Substitute.For<IProfile>();
+            _profileForTest.GetPassword( "ouch" ).Returns( "1978" );
+
+            _tokenForTest = Substitute.For<IToken>();
+            _tokenForTest.GetRandom( string.Empty ).ReturnsForAnyArgs( "000000" );
+
+            _authenticationService = new AuthenticationService( _profileForTest , _tokenForTest );
         }
 
         [Test]
         public void IsValidTest_Password_Is_Valid()
         {
-            //Arrange
-            var profileForTest = Substitute.For<IProfile>();
-            profileForTest.GetPassword( "ouch" ).Returns( "1978" );
-
-            var tokenForTest = Substitute.For<IToken>();
-            tokenForTest.GetRandom( string.Empty ).ReturnsForAnyArgs( "000000" );
-
-            _authenticationService = new AuthenticationService( profileForTest , tokenForTest );
-
-            //Action
-            var actual = _authenticationService.IsValid( "ouch" , "1978000000" );
-
-            //Assert
-            Assert.AreEqual( true , actual );
+            GivenAccountAndPassword( "ouch" , "1978000000" );
+            ShouldReturn( true );
         }
 
         [Test]
         public void IsValidTest_Password_Is_Invalid()
         {
-            //Arrange
-            var profileForTest = Substitute.For<IProfile>();
-            profileForTest.GetPassword( "ouch" ).Returns( "1978" );
-
-            var tokenForTest = Substitute.For<IToken>();
-            tokenForTest.GetRandom( string.Empty ).ReturnsForAnyArgs( "000000" );
-
-            _authenticationService = new AuthenticationService( profileForTest , tokenForTest );
-
-            //Action
-            var actual = _authenticationService.IsValid( "ouch" , "1234" );
-
-            //Assert
-            Assert.AreEqual( false , actual );
+            GivenAccountAndPassword( "ouch" , "1234" );
+            ShouldReturn( false );
         }
+
+        private void ShouldReturn( bool isValid )
+        {
+            Assert.AreEqual( isValid , _authenticationService.IsValid( _account , _password ) );
+        }
+
+        private void GivenAccountAndPassword( string account , string password )
+        {
+            _account = account;
+            _password = password;
+        }
+
     }
 }
