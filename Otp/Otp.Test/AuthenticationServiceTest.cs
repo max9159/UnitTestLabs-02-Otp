17eb663c6d3eecbd7495d3ac59b95e4bfd411ae6
Max Chou <max9159@gmail.com>
2019-06-17 23:28:10 +0800

    use IOC, add NSubstitute, add two tests

--- Otp/Otp.Test/AuthenticationServiceTest.cs
+++ Otp/Otp.Test/AuthenticationServiceTest.cs
@@ -2,6 +2,7 @@
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using System.Collections;
 using System.Collections.Generic;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Otp.Test
@@ -10,9 +11,45 @@ namespace Otp.Test
     public class AuthenticationServiceTest
     {
         [Test]
-        public void Try_invalid_account_password()
+        public void invalid_account_password()
         {
+            //arrange
+            var profile = Substitute.For<IProfile>();
+            profile.GetPassword("Max").Returns("9159");
 
+            var token = Substitute.For<IRsaTokenDao>();
+            token.GetRandom("Max").Returns("random");
+
+            var target = new AuthenticationService(profile, token);
+            const string account = "Max";
+            const string password = "tryError";
+
+            //act
+            var actual = target.IsValid(account, password);
+
+            // assert
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void success_account_password()
+        {
+            //arrange
+            var profile = Substitute.For<IProfile>();
+            profile.GetPassword("Max").Returns("9159");
+
+            var token = Substitute.For<IRsaTokenDao>();
+            token.GetRandom("Max").Returns("random");
+
+            var target = new AuthenticationService(profile, token);
+            const string account = "Max";
+            const string password = "9159random";
+
+            //act
+            var actual = target.IsValid(account, password);
+
+            // assert
+            Assert.IsTrue(actual);
         }
     }
 }
--- Otp/Otp/AuthenticationService.cs
+++ Otp/Otp/AuthenticationService.cs
@@ -2,15 +2,22 @@
 {
     public class AuthenticationService
     {
+        private IProfile _profile;
+        private IRsaTokenDao _token;
+
+        public AuthenticationService(IProfile profile, IRsaTokenDao token)
+        {
+            _profile = profile;
+            _token = token;
+        }
+
         public bool IsValid( string account , string password )
         {
             // 根據 account 取得自訂密碼
-            var profileDao = new ProfileDao();
-            var passwordFromDao = profileDao.GetPassword( account );
+            var passwordFromDao = _profile.GetPassword( account );
 
             // 根據 account 取得 RSA token 目前的亂數
-            var rsaToken = new RsaTokenDao();
-            var randomCode = rsaToken.GetRandom( account );
+            var randomCode = _token.GetRandom( account );
 
             // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
             var validPassword = passwordFromDao + randomCode;
